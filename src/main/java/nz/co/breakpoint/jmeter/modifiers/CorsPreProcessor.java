3e37c63a57063ea9fd41e8b7ef5e682cedab6179
Till Neunast <tilln@users.noreply.github.com>
2024-08-28 08:13:31 +1200

    Implement preflight cache

--- src/main/java/nz/co/breakpoint/jmeter/modifiers/CorsPreProcessor.java
+++ src/main/java/nz/co/breakpoint/jmeter/modifiers/CorsPreProcessor.java
@@ -1,12 +1,15 @@
 package nz.co.breakpoint.jmeter.modifiers;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Spliterators;
+import java.net.MalformedURLException;
+import java.time.Instant;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.StreamSupport;
 import java.util.stream.Collectors;
-
+import org.apache.commons.collections4.map.PassiveExpiringMap;
+import org.apache.commons.lang3.tuple.ImmutableTriple;
+import org.apache.jmeter.engine.event.LoopIterationEvent;
 import org.apache.jmeter.processor.PreProcessor;
 import org.apache.jmeter.protocol.http.control.Header;
 import org.apache.jmeter.protocol.http.control.HeaderManager;
@@ -17,6 +20,7 @@ import org.apache.jmeter.samplers.SampleResult;
 import org.apache.jmeter.samplers.Sampler;
 import org.apache.jmeter.testbeans.TestBean;
 import org.apache.jmeter.testelement.AbstractTestElement;
+import org.apache.jmeter.testelement.TestIterationListener;
 import org.apache.jmeter.testelement.ThreadListener;
 import org.apache.jmeter.testelement.property.JMeterProperty;
 import org.apache.jmeter.threads.JMeterContext;
@@ -25,7 +29,8 @@ import org.apache.jorphan.collections.SearchByClass;
 import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
 
-public class CorsPreProcessor extends AbstractTestElement implements PreProcessor, ThreadListener, TestBean {
+public class CorsPreProcessor extends AbstractTestElement
+        implements PreProcessor, ThreadListener, TestIterationListener, TestBean {
 
     private static final long serialVersionUID = 1L;
 
@@ -34,13 +39,25 @@ public class CorsPreProcessor extends AbstractTestElement implements PreProcesso
     protected transient ListenerNotifier notifier = new ListenerNotifier();
     protected transient List<SampleListener> listeners;
 
+    protected transient PassiveExpiringMap<ImmutableTriple<String, String, String>, Long> preflightCache = // cached value is simply the expiration time in epoch millis
+            new PassiveExpiringMap<>((PassiveExpiringMap.ExpirationPolicy<ImmutableTriple<String, String, String>, Long>)
+                    (key, value) -> value);
+
+
     public static final String PREFLIGHT_LABEL_SUFFIX = "preflightLabelSuffix";
+    public static final String CLEAR_EACH_ITERATION = "clearEachIteration";
+    public static final String DEFAULT_CACHE_EXPIRY = "defaultCacheExpiry";
+
     public static final String PREFLIGHT_LABEL_SUFFIX_DEFAULT = "-preflight";
     public static final String allowedMethods = "GET|HEAD|POST";
     public static final String forbiddenMethods = "CONNECT|TRACE|TRACK";
     public static final String methodOverrideHeaders = "x-http-method|x-http-method-override|x-method-override";
 
-    final static Map<String, String> safeListedHeaders = new HashMap<String, String>() {{
+    public static final Pattern MAX_AGE_HEADER_PATTERN = Pattern.compile("(?i)\\bAccess-Control-Max-Age: (\\V*)");
+    public static final Pattern ALLOW_HEADERS_HEADER_PATTERN = Pattern.compile("(?i)\\bAccess-Control-Allow-Headers: (\\V*)");
+    public static final Pattern ALLOW_METHODS_HEADER_PATTERN = Pattern.compile("(?i)\\bAccess-Control-Allow-Methods: (\\V*)");
+
+    public static final Map<String, String> safeListedHeaders = new HashMap<String, String>() {{
             put("accept", ".*");
             put("accept-language", ".*");
             put("content-language", ".*");
@@ -74,6 +91,10 @@ public class CorsPreProcessor extends AbstractTestElement implements PreProcesso
             "via"
     );
 
+    public static final String nonWildcardHeaders = String.join("|",
+            "authorization"
+    );
+
     @Override
     public void process() {
         JMeterContext context = getThreadContext();
@@ -85,28 +106,44 @@ public class CorsPreProcessor extends AbstractTestElement implements PreProcesso
 
         HTTPSamplerBase httpSampler = (HTTPSamplerBase) sampler;
         final String method = httpSampler.getMethod();
-        final String preflightHeaders = getPreflightHeaders(httpSampler.getHeaderManager());
+        final String url;
+        try {
+            url = String.valueOf(httpSampler.getUrl());
+        } catch (MalformedURLException e) {
+            log.error("Invalid sampler URL", e);
+            return;
+        }
+        List<String> preflightHeaders = getPreflightHeaders(httpSampler.getHeaderManager());
 
         if (method.matches(allowedMethods) && preflightHeaders.isEmpty()) return; // simple request
 
         HTTPSamplerBase preflight = (HTTPSamplerBase) sampler.clone();
         HeaderManager hm = new HeaderManager();
-
-        hm.add(new Header("Access-Control-Request-Method", method));
-        hm.add(new Header("Access-Control-Request-Headers", preflightHeaders));
         hm.removeHeaderNamed("Authorization");
+        hm.removeHeaderNamed("Accept");
+        hm.add(new Header("Accept", "*/*"));
+        hm.add(new Header("Access-Control-Request-Method", method));
+        hm.add(new Header("Access-Control-Request-Headers", String.join(",", preflightHeaders)));
 
         preflight.setHeaderManager(hm);
         preflight.setMethod("OPTIONS");
-        preflight.setName(preflight.getName()+getPreflightLabelSuffix());
+        preflight.setName(preflight.getName() + getPreflightLabelSuffix());
         preflight.setThreadContext(context);
         preflight.setThreadName(context.getThread().getThreadName());
 
+        if (isInPreflightCache(url, method, preflightHeaders)) {
+            log.debug("Preflight still cached, skipping.");
+            return;
+        }
         SampleResult result = preflight.sample();
+        addToPreflightCache(result);
         notifier.notifyListeners(new SampleEvent(result, context.getThreadGroup().getName()), listeners);
     }
 
-
+    /**
+     * @param h JMeter Header of the actual HTTP request
+     * @return true iff the header is one that requires a preflight request
+     */
     boolean isPreflightHeader(Header h) {
         final String name = h.getName().toLowerCase();
 
@@ -123,16 +160,64 @@ public class CorsPreProcessor extends AbstractTestElement implements PreProcesso
         return true;
     }
 
-    String getPreflightHeaders(HeaderManager hm) {
-        return hm == null ? ""
+    /**
+     * @param hm JMeter HeaderManager of the actual HTTP request
+     * @return list of header names to be sent in the
+     * "Access-Control-Request-Headers" preflight request header
+     */
+    List<String> getPreflightHeaders(HeaderManager hm) {
+        return (hm == null) ? Arrays.asList()
                 : StreamSupport.stream(Spliterators.spliteratorUnknownSize(hm.getHeaders().iterator(), 0), false)
                 .map(JMeterProperty::getObjectValue)
                 .map(Header.class::cast)
                 .filter(this::isPreflightHeader)
                 .map(Header::getName)
-                .collect(Collectors.joining(","));
+                .collect(Collectors.toList());
     }
 
+    protected boolean isInPreflightCache(String url, String method, List<String> headers) {
+        return (preflightCache.containsKey(ImmutableTriple.of(url, method.toUpperCase(),"")) ||
+                preflightCache.containsKey(ImmutableTriple.of(url, "*", ""))
+        ) && headers.stream().map(String::toLowerCase).allMatch(header ->
+                preflightCache.containsKey(ImmutableTriple.of(url, "", header)) ||
+                preflightCache.containsKey(ImmutableTriple.of(url, "", "*"))
+                        && !header.matches(nonWildcardHeaders));
+    }
+
+    protected void addToPreflightCache(SampleResult result) {
+        long maxAge = getMaxAge(result);
+        log.debug("Caching \"{}\" for {} seconds", result.getSampleLabel(), maxAge);
+        long expiry = Instant.now().plusSeconds(maxAge).toEpochMilli();
+        final String url = result.getUrlAsString();
+        getAllowHeaders(result).forEach(header ->
+                preflightCache.put(ImmutableTriple.of(url, "", header.toLowerCase()), expiry));
+        getAllowMethods(result).forEach(method ->
+                preflightCache.put(ImmutableTriple.of(url, method.toUpperCase(), ""), expiry));
+    }
+
+    /**
+     * @param result JMeter SampleResult of a preflight request
+     * @return duration (in seconds) until preflight expiry as per Access-Control-Max-Age response header,
+     * or the default of 5 seconds if the header is not received
+     */
+    public long getMaxAge(SampleResult result) {
+        final Matcher m = MAX_AGE_HEADER_PATTERN.matcher(result.getResponseHeaders());
+        return m.find() ? Long.parseLong(m.group(1)) : getDefaultCacheExpiry();
+    }
+
+    public List<String> getAllowHeaders(SampleResult result) {
+        final Matcher m = ALLOW_HEADERS_HEADER_PATTERN.matcher(result.getResponseHeaders());
+        return m.find() ? Arrays.asList(m.group(1).split("[\\s,]+")) : Arrays.asList();
+    }
+
+    public List<String> getAllowMethods(SampleResult result) {
+        final Matcher m = ALLOW_METHODS_HEADER_PATTERN.matcher(result.getResponseHeaders());
+        return m.find() ? Arrays.asList(m.group(1).split("[\\s,]+")) : Arrays.asList();
+    }
+
+    /** When the JMeter Thread is started, find all SampleListeners in Test Plan, so they can be notified
+     * when the preflight request is made.
+     */
     @Override
     public void threadStarted() {
         SearchByClass<SampleListener> listenersSearch = new SearchByClass<>(SampleListener.class);
@@ -143,7 +228,18 @@ public class CorsPreProcessor extends AbstractTestElement implements PreProcesso
     @Override
     public void threadFinished() {}
 
+    @Override
+    public void testIterationStart(LoopIterationEvent event) {
+        if (getClearEachIteration()) { // TODO JMeter 5.2 "same user on each iteration"
+            preflightCache.clear();
+        }
+    }
+
     public String getPreflightLabelSuffix() { return getPropertyAsString(PREFLIGHT_LABEL_SUFFIX); }
     public void setPreflightLabelSuffix(String suffix) { setProperty(PREFLIGHT_LABEL_SUFFIX, suffix); }
+    public long getDefaultCacheExpiry() { return getPropertyAsLong(DEFAULT_CACHE_EXPIRY); }
+    public void setDefaultCacheExpiry(long seconds) { setProperty(DEFAULT_CACHE_EXPIRY, seconds); }
+    public boolean getClearEachIteration() { return getPropertyAsBoolean(CLEAR_EACH_ITERATION); }
+    public void setClearEachIteration(boolean control) { setProperty(CLEAR_EACH_ITERATION, control); }
 
 }
--- src/main/java/nz/co/breakpoint/jmeter/modifiers/CorsPreProcessorBeanInfo.java
+++ src/main/java/nz/co/breakpoint/jmeter/modifiers/CorsPreProcessorBeanInfo.java
@@ -2,20 +2,33 @@ package nz.co.breakpoint.jmeter.modifiers;
 
 import java.beans.PropertyDescriptor;
 import org.apache.jmeter.testbeans.BeanInfoSupport;
-import static nz.co.breakpoint.jmeter.modifiers.CorsPreProcessor.PREFLIGHT_LABEL_SUFFIX;
-import static nz.co.breakpoint.jmeter.modifiers.CorsPreProcessor.PREFLIGHT_LABEL_SUFFIX_DEFAULT;
+
+import static nz.co.breakpoint.jmeter.modifiers.CorsPreProcessor.*;
 
 public class CorsPreProcessorBeanInfo extends BeanInfoSupport {
 
     public CorsPreProcessorBeanInfo() {
         super(CorsPreProcessor.class);
 
-        createPropertyGroup("CORS", new String[]{
-                PREFLIGHT_LABEL_SUFFIX
+        createPropertyGroup("Preflight", new String[]{
+                PREFLIGHT_LABEL_SUFFIX, CLEAR_EACH_ITERATION
         });
+        PropertyDescriptor p;
 
-        PropertyDescriptor p = property(PREFLIGHT_LABEL_SUFFIX);
+        p = property(PREFLIGHT_LABEL_SUFFIX);
         p.setValue(NOT_UNDEFINED, Boolean.TRUE);
         p.setValue(DEFAULT, PREFLIGHT_LABEL_SUFFIX_DEFAULT);
+
+        createPropertyGroup("Cache", new String[]{
+                CLEAR_EACH_ITERATION, DEFAULT_CACHE_EXPIRY
+        });
+
+        p = property(CLEAR_EACH_ITERATION);
+        p.setValue(NOT_UNDEFINED, Boolean.TRUE);
+        p.setValue(DEFAULT, Boolean.TRUE);
+
+        p = property(DEFAULT_CACHE_EXPIRY);
+        p.setValue(NOT_UNDEFINED, Boolean.TRUE);
+        p.setValue(DEFAULT, 5L);
     }
 }
